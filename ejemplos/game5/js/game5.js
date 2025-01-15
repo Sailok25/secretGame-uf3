612469a16806055d3b0d00ef677fc70e76f279f9
Sailok <albaayvi4@gmail.com>
2025-01-15 16:55:36 +0100

    moviment de pacman + puntuació

--- ejemplos/game5/js/game5.js
+++ ejemplos/game5/js/game5.js
@@ -40,6 +40,43 @@ let fantasma1 = new fantasma(10, 30);
 let fantasma2 = new fantasma(10, 100);
 let fantasma3 = new fantasma(10, 200);
 
+let heroi = function(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.velocitat = 4;
+    this.dibuixa = function(imatge){
+        pacmanImg = new Image();
+        pacmanImg.src = imatge;
+
+        ctx.drawImage(pacmanImg, this.x, this.y, 50, 50);
+    }
+
+    this.esquerra = function(){
+        this.x -= this.velocitat;
+    }
+
+    this.dreta = function(){
+        this.x += this.velocitat;
+    }
+
+    this.adalt = function(){
+        this.y -= this.velocitat;
+    }
+
+    this.abaix = function(){
+        this.y += this.velocitat;
+    }
+
+    this.puntuacio = function(){
+        ctx.font = "30px Arial";
+        ctx.fillStyle = "grey";
+        ctx.fillText("Score: " + this.x, 50, 50);
+    }
+}
+
+let pacman = new heroi(50, 50)
+
 function inici() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -50,6 +87,8 @@ function inici() {
 function principal() {
     esborrarCanvas();
 
+    pacman.dibuixa('pacman/pacman.png')
+    
     fantasma1.dibuixa('pacman/f-rojo.png');
     fantasma1.mou(5);
 
@@ -64,3 +103,22 @@ function esborrarCanvas() {
     canvas.width = 500;
     canvas.heigh = 300;
 }
+
+
+document.addEventListener('keydown', (tecla)=>{
+    if (tecla.key == 'ArrowLeft'){
+        pacman.esquerra();
+    }
+    
+    if (tecla.key == 'ArrowRight') {
+        pacman.dreta();
+    }
+
+    if (tecla.key == 'ArrowUp'){
+        pacman.adalt();
+    }
+
+    if (tecla.key == 'ArrowDown'){
+        pacman.abaix();    
+    }
+})
